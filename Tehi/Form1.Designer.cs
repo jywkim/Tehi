31f9fd2113f7b172342632be525e088b1e07d4a9
jywkim <jkim589@gmail.com>
2018-10-10 22:47:03 -0400

    add list box, status strip, deal button

--- Tehi/Form1.Designer.cs
+++ Tehi/Form1.Designer.cs
@@ -28,7 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.LogListBox = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.DealButton = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // LogListBox
+            // 
+            this.LogListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogListBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogListBox.FormattingEnabled = true;
+            this.LogListBox.HorizontalScrollbar = true;
+            this.LogListBox.ItemHeight = 16;
+            this.LogListBox.Location = new System.Drawing.Point(135, 171);
+            this.LogListBox.Name = "LogListBox";
+            this.LogListBox.Size = new System.Drawing.Size(112, 36);
+            this.LogListBox.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 359);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(649, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.StatusLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // DealButton
+            // 
+            this.DealButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.DealButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DealButton.Location = new System.Drawing.Point(280, 171);
+            this.DealButton.Name = "DealButton";
+            this.DealButton.Size = new System.Drawing.Size(96, 32);
+            this.DealButton.TabIndex = 2;
+            this.DealButton.Text = "Deal";
+            this.DealButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -36,14 +83,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(649, 381);
+            this.Controls.Add(this.DealButton);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.LogListBox);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "The Eyes Have It!";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox LogListBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.Button DealButton;
     }
 }
 
