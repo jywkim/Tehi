653ea97582f91a779d86b5aaff1ee1cb00927f6c
jywkim <jkim589@gmail.com>
2018-10-12 18:49:27 -0400

    add constructor that loops through suits/ranks

--- CardLib/CardDeck.cs
+++ CardLib/CardDeck.cs
@@ -9,5 +9,21 @@ namespace CardLib
     public class CardDeck
     {
         private Stack<PlayingCard> cardStack = new Stack<PlayingCard>();
+
+        public CardDeck()
+        {
+            Array suitChoices = Enum.GetValues(typeof(CardSuit));
+            foreach (CardSuit s in suitChoices)
+            {
+                for (int r = PlayingCard.Ace; r <= PlayingCard.King; r ++)
+                {
+                    PlayingCard card;
+                    if (r < PlayingCard.Jack) card = new PlayingCard(r, s);
+                    else card = new FaceCard(r, s);
+                    card.FaceUp = true;
+                    cardStack.Push(card);
+                }
+            }
+        }
     }
 }
