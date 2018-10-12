3f11cc265cb2666edcdecd987ad2668d238f2a14
jywkim <jkim589@gmail.com>
2018-10-11 21:18:42 -0400

    modify DealButton_Click() to create FaceCard

--- Tehi/Form1.cs
+++ Tehi/Form1.cs
@@ -20,7 +20,7 @@ namespace Tehi
 
         private void DealButton_Click(object sender, EventArgs e)
         {
-            LogListBox.Items.Clear();            PlayingCard card = new PlayingCard(PlayingCard.Queen, CardSuit.Clubs);            card.FaceUp = true;            LogListBox.Items.Add("Card: " + card);
+            LogListBox.Items.Clear();            FaceCard card = new FaceCard(FaceCard.Queen, CardSuit.Clubs);            card.FaceUp = true;            LogListBox.Items.Add("Card: " + card);
             LogListBox.Items.Add("Rank: " + card.Rank);            LogListBox.Items.Add("Suit: " + card.Suit);            LogListBox.Items.Add("Code:" + card.Code);
             LogListBox.Items.Add("Index:" + card.Index);            StatusLabel.Text = "Deal button pressed";        }
 
