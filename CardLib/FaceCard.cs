b9d70d70e82a6637d6c1b12e850caeb9a984cb53
jywkim <jkim589@gmail.com>
2018-10-11 21:16:44 -0400

    add FaceCard class with constructor

---
+++ CardLib/FaceCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLib
+{
+    public class FaceCard : PlayingCard
+    {
+        public int Eyes { get; private set; }
+
+        public FaceCard(int rank, CardSuit suit)
+            : base(rank, suit)
+        {
+            Eyes = 2;
+        }
+    }
+}
